a8d4ace9a80bb44a006317749870c3d791d55358
Mohammed Talib <talib@Mohammeds-Air.lan>
2024-09-13 02:26:32 +0100

    Corrected proc file

--- app/main.py
+++ app/main.py
@@ -26,3 +26,6 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
 
+@app.get('/')
+def root():
+    return {"message": "Hello, The App is running"}
